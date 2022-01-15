b2b8c46c29b705395ea46610152d14b070ce0746
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-15 23:04:01 +0700

    feat: add secret and math helper

---
+++ UserService/Helper/MathHelper.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Helper
+{
+    public class MathHelper
+    {
+        public double getDistanceFromLatLonInKm(double lat1, double lon1, double lat2, double lon2) {
+            var R = 6371; // Radius of the earth in km
+            var dLat = deg2rad(lat2-lat1);  // deg2rad below
+            var dLon = deg2rad(lon2-lon1); 
+            var a = 
+                Math.Sin(dLat/2) * Math.Sin(dLat/2) +
+                Math.Cos(deg2rad(lat1)) * Math.Cos(deg2rad(lat2)) * 
+                Math.Sin(dLon/2) * Math.Sin(dLon/2)
+                ; 
+            var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1-a)); 
+            var d = R * c; // Distance in km
+            return d;
+            }
+
+        public double deg2rad(double deg) {
+            return deg * (Math.PI/180);
+        }
+    }
+}
