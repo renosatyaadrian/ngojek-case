46d2aebdc720ee854b74060c0fc04c34f2112844
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-16 13:34:23 +0700

    fix user dtos

--- UserService/Dtos/AuthenticateDto.cs
+++ UserService/Dtos/AuthenticateDto.cs
@@ -7,6 +7,7 @@ namespace UserService.Dtos
 {
     public class AuthenticateDto
     {
-        
+        public string Username { get; set; }
+        public string Password { get; set; }
     }
 }
--- UserService/Dtos/CreateUserDto.cs
+++ UserService/Dtos/CreateUserDto.cs
@@ -8,6 +8,10 @@ namespace UserService.Dtos
     public class CreateUserDto
     {
         public string Username { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
     }
 }
