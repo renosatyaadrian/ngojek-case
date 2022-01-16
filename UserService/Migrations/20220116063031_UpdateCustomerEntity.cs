b6c129aa0ef79ce75674318372cd65ff60a948e4
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-16 13:34:11 +0700

    add user entity update migration

---
+++ UserService/Migrations/20220116063031_UpdateCustomerEntity.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserService.Migrations
+{
+    public partial class UpdateCustomerEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserLatitude",
+                table: "Customers");
+
+            migrationBuilder.DropColumn(
+                name: "UserLongitude",
+                table: "Customers");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "PhoneNumber",
+                table: "Customers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "UserLatitude",
+                table: "Customers",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "UserLongitude",
+                table: "Customers",
+                type: "float",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
