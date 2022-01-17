906b6034423d736cba81a7cefbc21ea6556dff9e
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-17 09:57:12 +0700

    add migrations

---
+++ UserService/Migrations/20220117025000_UpdateEntity.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace UserService.Migrations
+{
+    public partial class UpdateEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Drivers",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Username",
+                table: "Customers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Drivers");
+
+            migrationBuilder.DropColumn(
+                name: "Username",
+                table: "Customers");
+        }
+    }
+}
