e7184a726e5f54e53cf4d7081984878aa69286ee
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-19 10:17:01 +0700

    add math helper

--- UserService/Helper/MathHelper.cs
+++ UserService/Helper/MathHelper.cs
@@ -8,7 +8,7 @@ namespace UserService.Helper
 {
     public static class MathHelper
     {
-        public static double getDistanceFromLatLonInKm(double lat1, double lon1, double lat2, double lon2) {
+        public static float getDistanceFromLatLonInKm(double lat1, double lon1, double lat2, double lon2) {
             var R = 6371; // Radius of the earth in km
             var dLat = deg2rad(lat2-lat1);  // deg2rad below
             var dLon = deg2rad(lon2-lon1); 
@@ -19,7 +19,7 @@ namespace UserService.Helper
                 ; 
             var c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1-a)); 
             var d = R * c; // Distance in km
-            return d;
+            return (float)d;
             }
 
         public static double deg2rad(double deg) {
@@ -29,6 +29,11 @@ namespace UserService.Helper
         public static string ToRupiah(double angka)
         {
             return String.Format(CultureInfo.CreateSpecificCulture("id-id"), "Rp. {0:N}", angka);
+        }
+
+        public static float DistanceRounding(float distance)
+        {
+            return (float) Math.Round(distance, MidpointRounding.AwayFromZero);
         }
     }
 }
