be18d42dfbe28abcb4ada68e830c8f65d385d78c
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-20 20:28:00 +0700

    add http client to order service

--- UserService/SyncDataServices/Http/HttpOrderDataClient.cs
+++ UserService/SyncDataServices/Http/HttpOrderDataClient.cs
@@ -2,9 +2,11 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Text.Json;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using UserService.Dtos;
@@ -16,25 +18,26 @@ namespace UserService.SyncDataServices.Http
     {
         private readonly HttpClient _httpClient;
         private readonly IConfiguration _configuration;
+        private readonly IHttpContextAccessor _httpContextAccessor;
 
         public HttpOrderDataClient()
         {
         }
 
-        public HttpOrderDataClient(HttpClient httpClient, IConfiguration configuration)
+        public HttpOrderDataClient(HttpClient httpClient, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
         {
             _httpClient = httpClient;
             _configuration = configuration;
+            _httpContextAccessor = httpContextAccessor;
         }
 
         public async Task<OrderDto> GetUserOrderById(int id, int orderId)
         {
-            var httpContent = new StringContent(
-                JsonSerializer.Serialize(String.Empty),
-                Encoding.UTF8, "application/json"
-            );
-            HttpResponseMessage response = await _httpClient.PostAsync(
-                $"http://localhost:7000/api/User/{id}/Order/{orderId}", httpContent);
+            string tokenHeader = _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
+            string token = tokenHeader.Substring("Bearer ".Length).Trim();
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage response = await _httpClient.GetAsync(
+                $"http://localhost:7000/api/User/{id}/Order/{orderId}");
             // var response = await _httpClient.PostAsync(_configuration.GetSection("OrderService").GetValue<string>("GetUserOrderById"), httpContent);
             response.EnsureSuccessStatusCode();
             string responseBody = await response.Content.ReadAsStringAsync();
@@ -52,12 +55,12 @@ namespace UserService.SyncDataServices.Http
 
         public async Task<IEnumerable<OrderDto>> GetUserOrdersHistory(int id)
         {
-            var httpContent = new StringContent(
-                JsonSerializer.Serialize(String.Empty),
-                Encoding.UTF8, "application/json"
-            );
-            HttpResponseMessage response = await _httpClient.PostAsync(
-                $"http://localhost:7000/api/User/{id}/Orders", httpContent);
+            string tokenHeader = _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
+            string token = tokenHeader.Substring("Bearer ".Length).Trim();
+
+            _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage response = await _httpClient.GetAsync(
+                $"http://localhost:7000/api/User/{id}/Orders");
             // var response = await _httpClient.PostAsync(_configuration.GetSection("OrderService").GetValue<string>("GetUserOrderById"), httpContent);
             response.EnsureSuccessStatusCode();
             string responseBody = await response.Content.ReadAsStringAsync();
