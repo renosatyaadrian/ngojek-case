bc5d7ef39c216043b1bfa8182b3a85c84eb709c2
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-20 09:46:28 +0700

    remove unused code

--- OrderService/Controllers/OrdersController.cs
+++ OrderService/Controllers/OrdersController.cs
@@ -1,9 +1,6 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 using AutoMapper;
-// using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 using OrderService.Data;
 using OrderService.Dtos;
