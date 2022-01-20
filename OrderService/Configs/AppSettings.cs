0b073fe6b83282bcaa28efe97f86a2a643aded1c
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-20 17:00:03 +0700

    add configs

---
+++ OrderService/Configs/AppSettings.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderService.Configs
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
