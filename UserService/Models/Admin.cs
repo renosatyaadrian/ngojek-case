f24b694dca6247e8952011f7efef38192877fa44
renosatyaadrian <renosatyaadrian@gmail.com>
2022-01-23 21:32:26 +0700

    add admin entity

---
+++ UserService/Models/Admin.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Models
+{
+    public class Admin
+    {
+        [Key]
+        [Required]
+        public int Id { get; set; }
+        [Required]
+        public string FullName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required] 
+        public string Username { get; set; }
+    }
+}
